310179049d842bb4f0ef305c9590a4589ec59e9c
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2021-07-16 17:24:52 +0100

    modify development version to comprise build count

--- continuous_delivery_scripts/generate_news.py
+++ continuous_delivery_scripts/generate_news.py
@@ -85,9 +85,10 @@ def _update_version_string(
         version_elements: version elements
     """
     if commit_type == CommitType.DEVELOPMENT:
-        return "%s-%s+%s" % (
+        return "%s-%s.%s+%s" % (
             new_version,
             auto_version_tool.config.BUILD_TOKEN,
+            version_elements.get(auto_version_tool.Constants.COMMIT_COUNT_FIELD),
             version_elements.get(auto_version_tool.Constants.COMMIT_FIELD),
         )
     return new_version
